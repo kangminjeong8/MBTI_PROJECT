fcf2566c6a44ad93f47b1a2e400c15ece0e5d9a7
amor4330 <amor4330@gmail.com>
2022-12-09 15:43:55 +0900

    minjae

---
+++ MBTI_Project/src/main/java/mbti_controller/board_controller.java
@@ -0,0 +1,45 @@
+package mbti_controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("*.board")
+public class board_controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
+	}
+	
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8 ");
+		
+		String uri = request.getRequestURI();
+		String path = request.getContextPath();
+		String command = uri.substring(path.length());
+		
+		System.out.println("요청 경뢰: " + command);
+		
+		//세션에서 ID 받아오기
+		HttpSession session = request.getSession();
+		
+		
+	
+		
+		
+		
+	}
+
+}
